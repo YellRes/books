ee18b94fefc530efbfd7ce4ee26917f5e1224af1
tianrui.zhang <tianrui.zhang@ly.com>
2021-03-12 18:18:35 +0800

    add:

--- structureIKonw/treeLike.js
+++ structureIKonw/treeLike.js
@@ -34,6 +34,7 @@ class Tree {
       } else {
         previousNode.right = newNode
       }
+      return 
     }
 
     if (currentNode.value > value) {
@@ -44,14 +45,41 @@ class Tree {
   } 
 
   // 遍历
-  traverse() {
+  traverse(node) {
+    if (!node) {
+      return
+    }
+    console.log(node.value)
+    this.traverse(node.left)
+    this.traverse(node.right)
   }
+
+  lastTraverse(node) {
+    if (!node) {
+      return
+    }
+
+    console.log(node.value)
+    this.lastTraverse(node)
+    this.lastTraverse(node.left)    
+  }
+
+
 }
 
 const tree1 = new Tree()
+
 tree1.add(1)
+tree1.add(-1)
 tree1.add(2)
 tree1.add(3)
+tree1.add(3.5)
+tree1.add(4)
+tree1.add(4.5)
 
-console.log(tree1);
+// console.dir(tree1, {depth: 100});
+tree1.traverse(tree1.root)
+tree1.lastTraverse(tree1.root)
+
+
 
