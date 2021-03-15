0fe3aecebb968549e325d6c6d977a31c7f26d34b
Yellres <2929799602@qq.com>
2021-03-15 22:54:49 +0800

    add: 链表方法

--- structureIKonw/linkList.js
+++ structureIKonw/linkList.js
@@ -9,15 +9,26 @@ class Node {
 
 class LinkList {
   constructor() {
+    this.count = 0
     this.head = null 
   } 
 
   // 最后一位添加
-  add(value) {
+  push(value) {
     const node = new Node(value)
+
+    if (!this.head) {
+      this.head = node
+    } else {
+      let current = this.head
+      while(current.next != null) {
+        current = current.next
+      }
+
+      current.next = node
+    }
     
-    const lastNode = this.findLastNode()
-    lastNode.next = node
+    this.count++
     
   }
   
@@ -33,19 +44,60 @@ class LinkList {
     return this.head
   }
 
-  // 遍历链表
-  traverse() {
-    while(this.head.next) {
+  insert(value, position) {
+  }
 
+  getElementAt(index) {
+  }
+
+  remove(value) {
+    if (!this.isEmpty()) {
+      // 1. 链表为空
+      throw 'the stack is empty'
+    } else if (this.head.value === value) {
+      // 2. 链表不为空
+      // 2.1 移除的是链表的头指针的元素
+      this.head = this.head.next
+      return --this.count
+    } else {
+      // 2.2 移除的不是头指针元素
+      let current = this.head
+      let preNode = null
+      while(current !== null) {
+        if (current.value === value) {
+          preNode.next = current.next
+          return --this.count
+        }
+        preNode = current
+        current = current.next
+      } 
     }
   }
 
-  findNumber() {
+  indexOf(value) {}
 
+  removeAt(position) {}
+
+  isEmpty() {
+    return this.head
   }
 
-  insertNode() {
+  size() {}
 
-  }
+  toString() {}
 
 }
+
+
+const ll = new LinkList()
+ll.push(15)
+ll.push(10)
+ll.push(9)
+ll.push(19)
+ll.push(18)
+
+console.dir(ll, {depth: 10});
+
+ll.remove(18)
+console.dir(ll, {depth: 10});
+
